d389c036582e7f135b493e0ebcc9ce57d81cc7a4
umidkhan <umidxondeveloper@mail.ru>
2022-12-02 11:14:04 +0500

    section1 created and ended

--- examproject/src/App.js
+++ examproject/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "./Components/Header/Header";
 import Navbar from "./Components/Navbar/Navbar";
+import Section1 from "./Components/Section_1/Section_1";
 
 export default function App() {
   return (
     <>
       <Navbar />
       <Header />
+      <Section1 />
     </>
   );
 }
